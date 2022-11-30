ae682c8cda1f87bcce63681b1317ed11755d75ed
PeerHerholz <PeerHerholz@users.noreply.github.com>
2022-11-30 16:28:18 +0000

    update README.rst bb5359ff28074221d404d73a57fa8fb07a1bfad6

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.post1+gab8e52b',
+    VERSION: '0.post1+gbb5359f',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
