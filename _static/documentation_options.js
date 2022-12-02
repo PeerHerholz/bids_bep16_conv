6216b86d41d1e781f94d80fd54d98db3e1b8ee31
PeerHerholz <PeerHerholz@users.noreply.github.com>
2022-12-02 11:19:45 +0000

    add function to download HBN data
    This commit adds a function to download the HBN DWI derivatives obtained via QSIprep from the BIDS connectivity project OSF repo. 0c5116cdc5e3520ec4fefb53243cc3e17a9966fd

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.post1+gbb5359f',
+    VERSION: '0.post1+g0c5116c',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
