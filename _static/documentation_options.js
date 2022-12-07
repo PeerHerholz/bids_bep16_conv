8dca95effa24d2ec6e60e2890bfe748cca3ca521
PeerHerholz <PeerHerholz@users.noreply.github.com>
2022-12-07 19:58:17 +0000

    add missing json metadata to HBN example dataset
    This commit/PR adds functionality to download the missing json files for the HBN example dataset. ef7de32149009adef10297090c561e51d8169a3f

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.post1+g3db797f',
+    VERSION: '0.post1+gef7de32',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
