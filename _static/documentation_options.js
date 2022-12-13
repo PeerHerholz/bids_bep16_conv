5f7323c06eedbd9128ab83aaca0be736eaa82c33
PeerHerholz <PeerHerholz@users.noreply.github.com>
2022-12-13 16:27:58 +0000

    update sphinx theme 4fe57cc83c8ed6a6adb8fb525319d91c78bdfc6f

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.post1+gf521143',
+    VERSION: '0.post1+g4fe57cc',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
