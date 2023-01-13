3413ba11e62b59b9903eaa444f3b268b1a535991
PeerHerholz <PeerHerholz@users.noreply.github.com>
2023-01-13 10:50:12 +0000

    add download function to CLI
    This commit adds the download functionality to the CLI via dedicated arguments. 75fc9c3e341541c4326b82fbd6752cc1700acf0a

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.post1+g12668a7',
+    VERSION: '0.post1+g75fc9c3',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
