5e34b1d995431ad8c4bba772be2d2ca1df520ad6
PeerHerholz <PeerHerholz@users.noreply.github.com>
2023-01-18 15:31:34 +0000

    fix build errors d779ea4f71677ce370b3e6de16da0ff6b3cfe553

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.post1+g8169379',
+    VERSION: '0.post1+gd779ea4',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
