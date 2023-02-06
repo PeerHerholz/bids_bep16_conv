459fb907b3ec2d141164e66287d9da509b804a94
PeerHerholz <PeerHerholz@users.noreply.github.com>
2023-02-06 08:33:18 +0000

    fix some errors and typos in the installation doc 5aa4a9b1ebe06ac5c59d14d175a12b85518a1eee

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.post1+ga78e1aa',
+    VERSION: '0.post1+g5aa4a9b',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
