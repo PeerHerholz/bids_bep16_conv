1525c1f3a266a053b159835e082ce4ced623015f
PeerHerholz <PeerHerholz@users.noreply.github.com>
2023-02-08 13:29:05 +0000

    update metadata templates 426fb436791d1219e3ad7422c9991adc9194fb60

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.post1+g5aa4a9b',
+    VERSION: '0.post1+g426fb43',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
