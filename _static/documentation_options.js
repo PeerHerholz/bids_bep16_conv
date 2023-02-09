2ae9bf93b35b0cf2f63dc4e40c6b2cbeda80bdf2
PeerHerholz <PeerHerholz@users.noreply.github.com>
2023-02-09 15:01:23 +0000

    Merge remote-tracking branch 'origin/main' into main 73e6c6021c0428c7890a3e8967fc208d48c5ddf5

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.post1+gfb1dff1',
+    VERSION: '0.post1+g73e6c60',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
